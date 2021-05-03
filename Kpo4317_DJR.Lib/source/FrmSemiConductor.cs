2eba1cf12c9dc005136b91863ad884c1fbaab6e4
Sleddie <jdelmuhametov@gmail.com>
2021-05-03 21:35:12 +0300

    Initial commit

---
+++ Kpo4317_DJR.Lib/source/FrmSemiConductor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kpo4317_DJR.Lib
+{
+    public partial class FrmSemiConductor : Form
+    {
+        public FrmSemiConductor()
+        {
+            InitializeComponent();
+        }
+
+        private SemiConductor _semiconductor = null;
+
+        public SemiConductor semiconductor
+        {
+            get { return _semiconductor; }
+        }
+
+        public void SetSemiconductor(SemiConductor semiconductor)
+        {
+            _semiconductor = semiconductor;
+            txtboxSubstance_Name.Text = _semiconductor.Substance_name;
+            txtboxForbiddenZone_width.Text = _semiconductor.ForbiddenZone_width.ToString();
+            txtboxElectron_move.Text = _semiconductor.Electron_move.ToString();
+            txtboxHole_move.Text = _semiconductor.Hole_move.ToString();
+        }
+    }
+}
---
+++ Kpo4317_DJR.Main/FrmMain.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Kpo4317_DJR.Lib;
+
+namespace Kpo4317_DJR.Main
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            txtboxLogPath.Text = AppGlobalSettings.LogPath;
+            txtboxDataFileName.Text = AppGlobalSettings.DataFileName;
+        }
+
+        private List<SemiConductor> semiconductorList = new List<SemiConductor>();
+        private BindingSource bsSemiConductors = new BindingSource();
+
+        private void LoadingRow(int currentStatus)
+        {
+            ListLoadingProgressBar.Value += currentStatus;
+        }
+
+        private void mnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void mnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ISemiconductorListLoader loader = CastleWindsorContainer.container.Resolve<ISemiconductorListLoader>(AppGlobalSettings.ImportSetting);
+                loader.LoadingRow = LoadingRow;
+                loader.Execute();
+                ListLoadingProgressBar.Value = ListLoadingProgressBar.Maximum;
+                bsSemiConductors.DataSource = loader.semiconductorList;
+                dgvSemiConductors.DataSource = bsSemiConductors;
+            }
+            //обработка исключения "Метод не реализован"
+            catch (NotImplementedException ex)
+            {
+                MessageBox.Show("Ошибка №1: " + ex.Message);
+                LogUtility.ErrorLog("Ошибка №1: " + ex.Message);
+
+            }
+            //обработка остальных исключений
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка №2: " + ex.Message);
+                LogUtility.ErrorLog(ex);
+            }
+        }
+
+        private void mnOpenSemiconductor_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FrmSemiConductor frmSemiConductor = new FrmSemiConductor();
+
+                SemiConductor semiconductor = (bsSemiConductors.Current as SemiConductor);
+                frmSemiConductor.SetSemiconductor(semiconductor);
+
+                frmSemiConductor.ShowDialog();
+            }
+            //обработка исключения "Метод не реализован"
+            catch (NotImplementedException ex)
+            {
+                MessageBox.Show("Ошибка №1: " + ex.Message);
+                LogUtility.ErrorLog("Ошибка №1: " + ex.Message);
+
+            }
+        }
+    }
+}
